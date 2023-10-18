72a9ded46c37ebba8eca625b5752e08fcdec3182
whatchang <dxt7469@gmail.com>
2023-10-18 20:22:28 +0900

    add base classifier

--- new_idea_base.py
+++ new_idea_base.py
@@ -160,6 +160,8 @@ class new_idea_vae(nn.Module):
         output = self.norm_layer2(output)
         output = self.leaky_relu(output)
 
+        output = self.classifier(output)
+
         return output
 
 
@@ -190,14 +192,10 @@ temperature = 1
 use_wandb = False
 use_scheduler = True
 scheduler_name = 'LROn'
-early_stopping = EarlyStopping(patience=50, verbose=True, path='best_classifier_model.pt')  # 초기화
+early_stopping = EarlyStopping(patience=50, verbose=True, path='best_base_classifier_model.pt')  # 초기화
 #lr_lambda = 0.97
 
 new_model = new_idea_vae('./result/Cross_vae_Linear_origin_b64_lr1e-3_4.pt').to('cuda')         #Cross_vae_Linear_origin_b64_lr1e-3_4.pt이게 뭔지 확인!
-new_model.load_state_dict(torch.load('result/number1.pt'))
-for param in new_model.parameters():
-    param.requires_grad = False
-new_model.classifier = nn.Linear(128, 400).to('cuda')
 #train_dataset_name = 'data/train_data.json'
 #valid_dataset_name = 'data/valid_data.json'
 train_dataset_name = 'data/train_new_idea.json'
@@ -291,7 +289,6 @@ for epoch in tqdm(range(epochs)):
         task = task.to(torch.long).to('cuda') # TODO 고치기
 
         output = new_model(input, output)
-        output = new_model.classifier(output)
 
         loss = criteria(output, task)
 
@@ -348,5 +345,5 @@ for epoch in tqdm(range(epochs)):
     # if use_scheduler:
     #     scheduler.step(avg_valid_loss)
 
-new_model.load_state_dict(torch.load('best_classifier_model.pt'))
-torch.save(new_model.state_dict(), f'result/classifier_number_{best_acc:.2f}.pt')
+new_model.load_state_dict(torch.load('best_base_classifier_model.pt'))
+torch.save(new_model.state_dict(), f'result/base_classifier_number_{best_acc:.2f}.pt')
