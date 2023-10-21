97c06a3babc3302f7105925704ad22b582cde3f4
whatchang <dxt7469@gmail.com>
2023-10-21 23:30:17 +0900

    add Concept ARC version And Optuna for auto-searching hypermarameter

--- new_idea_Concept_classifier .py
+++ new_idea_Concept_classifier .py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-from dataset import ARCDataset
+from dataset import ARCDataset, ARC_ValidDataset
 from tqdm import tqdm
 import torch.optim as optim
 from lion_pytorch import Lion
@@ -54,8 +54,8 @@ def seed_fix(seed):
     cudnn.deterministic = True
     random.seed(seed)
 
-def set_wandb(target_name, mode, seed, optimizer, dataset='ARC'):
-    run = wandb.init(project=f'{dataset}_{target_name}_{mode}', entity='gyojoongu')
+def set_wandb(target_name, mode, seed, optimizer, dataset='ARC', entity='gyojoongu'):
+    run = wandb.init(project=f'{dataset}_{target_name}_{mode}', entity=entity)
     if mode == 'train':
         config = {
             'learning_rate': lr,
@@ -160,6 +160,7 @@ class new_idea_vae(nn.Module):
 
         return output
 
+
 def label_making(task):
     task = task.tolist()
     label_index_list = []
@@ -174,12 +175,13 @@ def label_making(task):
 
     return torch.tensor(label_list, dtype=torch.long)
 
-
+entity = 'whatchang'
+permute_mode = True
 train_batch_size = 128
 valid_batch_size = 16
-lr = 1e-4
+lr = 1e-2
 batch_size = train_batch_size
-epochs = 1000
+epochs = 200
 seed = 777
 model_name = 'vae'
 mode = 'task'
@@ -187,37 +189,87 @@ temperature = 1
 use_wandb = False
 use_scheduler = True
 scheduler_name = 'LROn'
-early_stopping = EarlyStopping(patience=100, verbose=True, path='best_model.pt')  # 초기화
+early_stopping = EarlyStopping(patience=40, verbose=True, path='best_concept_classifier_model.pt')  # 초기화
 #lr_lambda = 0.97
 
 new_model = new_idea_vae('./result/Cross_vae_Linear_origin_b64_lr1e-3_4.pt').to('cuda')         #Cross_vae_Linear_origin_b64_lr1e-3_4.pt이게 뭔지 확인!
 #train_dataset_name = 'data/train_data.json'
 #valid_dataset_name = 'data/valid_data.json'
-train_dataset_name = 'data/train_new_idea.json'
-valid_dataset_name = 'data/valid_new_idea.json'
+train_dataset_name = 'data/train_concept.json'
+valid_dataset_name = 'data/test_concept.json'
 # train_dataset_name = 'data/train_new_idea_task_sample2_.json'
 # valid_dataset_name = 'data/valid_new_idea_task_sample2_.json'
-train_dataset = ARCDataset(train_dataset_name, mode=mode)
-valid_dataset = ARCDataset(valid_dataset_name, mode=mode)
+train_dataset = ARCDataset(train_dataset_name, mode=mode, permute_mode=permute_mode)
+valid_dataset = ARC_ValidDataset(valid_dataset_name, mode=mode)
 kind_of_dataset = 'Concept_task_sample2' if 'concept' in train_dataset_name else 'ARC_task_sample2' if 'sample2' in train_dataset_name else 'ARC'
 train_loader = DataLoader(train_dataset, batch_size=train_batch_size, drop_last=True, shuffle=True)
-valid_loader = DataLoader(valid_dataset, batch_size=valid_batch_size, drop_last=True, shuffle=True)
+valid_loader = DataLoader(valid_dataset, batch_size=valid_batch_size, drop_last=True, shuffle=False)
 
 #optimizer = optim.AdamW(new_model.parameters(), lr=lr)
 optimizer = Lion(new_model.parameters(), lr=lr, weight_decay=1e-2)
 #optimizer = optim.Adam(new_model.parameters(), lr=lr, weight_decay=5e-4)
 #scheduler = optim.lr_scheduler.LambdaLR(optimizer=optimizer, lr_lambda=lambda epoch: lr_lambda ** epoch)
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5, factor=0.5, verbose=True)
+#scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5, factor=0.5, verbose=True)
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.5, verbose=True)
+criteria = nn.CrossEntropyLoss()
 
-
+best_acc = 0
 seed_fix(seed)
 
+knn_model = KNeighborsClassifier(n_neighbors=5)
+
+train_embeddings = []  # 학습 데이터의 임베딩을 저장하기 위한 리스트
+train_labels = []      # 학습 데이터의 레이블을 저장하기 위한 리스트
+
+new_model.eval()
+for input, output, x_size, y_size, task in train_loader:
+    input = input.to(torch.float32).to('cuda')
+    output = output.to(torch.float32).to('cuda')
+    task = task.to(torch.long)
+    
+    with torch.no_grad():
+        embeddings = new_model(input, output)
+    train_embeddings.append(embeddings.cpu().numpy())
+    train_labels.extend(task.numpy())
+
+# 임베딩과 레이블의 형태를 조절합니다.
+train_embeddings = np.vstack(train_embeddings)
+train_labels = np.array(train_labels)
+
+# KNN 모델 학습
+knn_model.fit(train_embeddings, train_labels)
+
+valid_embeddings = []  # 검증 데이터의 임베딩을 저장하기 위한 리스트
+valid_labels = []      # 검증 데이터의 레이블을 저장하기 위한 리스트
+for input, output, x_size, y_size, task in valid_loader:
+    input = input.to(torch.float32).to('cuda')
+    output = output.to(torch.float32).to('cuda')
+    task = task.to(torch.long)
+    
+    with torch.no_grad():
+        embeddings = new_model(input, output)
+    valid_embeddings.append(embeddings.cpu().numpy())
+    valid_labels.extend(task.numpy())
+
+# 임베딩과 레이블의 형태를 조절합니다.
+valid_embeddings = np.vstack(valid_embeddings)
+valid_labels = np.array(valid_labels)
+
+# KNN의 정확도를 계산합니다.
+accuracy = knn_model.score(valid_embeddings, valid_labels)
+print(f'Before KNN Accuracy: {accuracy * 100:.2f}%')
+
+new_model.load_state_dict(torch.load('result/number1_inf.pt'))
+for param in new_model.parameters():
+    param.requires_grad = False
+new_model.classifier = nn.Linear(128, 16).to('cuda')
+
 if use_wandb:
-    set_wandb('new_idea', 'train', seed, 'Lion', kind_of_dataset)
+    set_wandb('new_idea', 'train', seed, 'Lion', kind_of_dataset, entity)
 
 
 def nt_xent_loss(output, temperature):
-    batch_size = output.shape[0]  
+    batch_size = output.shape[0]
     logits = torch.mm(output, output.t().contiguous()) / temperature
     labels = torch.arange(batch_size).to(output.device)
     loss = nn.CrossEntropyLoss()(logits, labels)
@@ -265,7 +317,7 @@ valid_labels = np.array(valid_labels)
 
 # KNN의 정확도를 계산합니다.
 accuracy = knn_model.score(valid_embeddings, valid_labels)
-print(f'KNN Accuracy: {accuracy * 100:.2f}%')
+print(f'After KNN Accuracy: {accuracy * 100:.2f}%')
 
 for epoch in tqdm(range(epochs)):
     train_total_loss = []
@@ -282,59 +334,63 @@ for epoch in tqdm(range(epochs)):
         task = task.to(torch.long).to('cuda') # TODO 고치기
 
         output = new_model(input, output)
+        output = new_model.classifier(output)
 
-        output_norm = output / output.norm(dim=-1).unsqueeze(1)
-        logits_pred = torch.matmul(output_norm, output_norm.T) * torch.exp(torch.tensor(temperature))
-
-        # labels = torch.tensor(np.arange(batch_size)).to('cuda')
-        labels = label_making(task).to('cuda')
-        #loss = nn.functional.nll_loss(nn.functional.log_softmax(logits_pred,dim=1), labels.to(torch.long))
-        loss = nt_xent_loss(output, temperature)        #NT-Xent Loss 사용
+        loss = criteria(output, task)
 
         loss.backward()
         optimizer.step()
         optimizer.zero_grad()
         train_total_loss.append(loss)
     print(f'train loss: {sum(train_total_loss) / len(train_total_loss)}')
+    # print("train loss: {0}, lr: {1:.6f}".format(sum(train_total_loss) / len(train_total_loss), optimizer.param_groups[0]['lr']))
+    if use_scheduler:
+        scheduler.step()
 
     if use_wandb:
         wandb.log({
             "train_loss": sum(train_total_loss) / len(train_total_loss),
         }, step=epoch)
 
+    acc = 0
     new_model.eval()
     for input, output, x_size, y_size, task in valid_loader:
         valid_count += valid_batch_size
-        input = input.to(torch.float32).to('cuda')
-        output = output.to(torch.float32).to('cuda')
-        task = task.to(torch.long)
+        input = input.to('cuda')
+        output = output.to('cuda')
+        task = task.to(torch.long).to('cuda')  # Moved the task tensor to the GPU as well
 
         output = new_model(input, output)
-        output_norm = output / output.norm(dim = -1).unsqueeze(1)
-        logits_pred = torch.matmul(output_norm, output_norm.T) * torch.exp(torch.tensor(temperature))
+        output = new_model.classifier(output)
 
-        labels = label_making(task).to('cuda')
-        #loss = nn.functional.nll_loss(nn.functional.log_softmax(logits_pred, dim=1), labels.to(torch.long))
-        loss = nt_xent_loss(output, temperature)        #NT-Xent Loss 사용
+        loss = criteria(output, task)
 
-        valid_total_loss.append(loss)
+        valid_total_loss.append(loss) # To store the loss value and not the tensor
+        output_soft = nn.functional.softmax(output, dim=1) # Specified dimension for softmax
+        output_argmax = torch.argmax(output_soft, dim=1)   # Specified dimension for argmax
+
+        acc += output_argmax.eq(task).sum() # Sum over the correct predictions
 
     avg_valid_loss = sum(valid_total_loss) / len(valid_total_loss)
 
-    early_stopping(avg_valid_loss, new_model)
-    if early_stopping.early_stop:
-        print("Early stopping")
-        break
+    # early_stopping(avg_valid_loss, new_model)
+    # early_stopping(-100 * acc/len(valid_dataset), new_model)
+    # if early_stopping.early_stop:
+    #     print("Early stopping")
+    #     break
 
     print(f'valid loss: {avg_valid_loss}')
+    print(f'valid accuracy: {100 * acc/len(valid_dataset):.2f}% ({acc}/{len(valid_dataset)})')
+    if best_acc < 100 * acc/len(valid_dataset):
+        best_acc = 100 * acc/len(valid_dataset)
 
     if use_wandb:
         wandb.log({
             "valid_loss": avg_valid_loss,
         }, step=epoch)
 
-    if use_scheduler:
-        scheduler.step(avg_valid_loss)
+    # if use_scheduler:
+    #     scheduler.step(avg_valid_loss)
 
-new_model.load_state_dict(torch.load('best_model.pt'))
-torch.save(new_model.state_dict(), f'result/number1.pt')
+new_model.load_state_dict(torch.load('best_concept_classifier_model.pt'))
+torch.save(new_model.state_dict(), f'result/concept_classifier_number_{best_acc:.2f}.pt')
--- prepreocess/concept_preprocessing.py
+++ prepreocess/concept_preprocessing.py
@@ -4,102 +4,187 @@ import json
 import numpy as np
 import re
 import torch
+from tqdm import tqdm
 
-dataset_path = '../data'
-original_path = '../original_data'
-test_path = f'{original_path}/corpus/*'
+dataset_path = 'data'
+original_path = 'original_data'
+train_path = f'{original_path}/concept_train/*'
+train_files = glob(train_path)
+test_path = f'{original_path}/concept_eval/*'
 test_files = glob(test_path)
 
 
 test_auto_dataset = []
 test_auto_size = []
-test_auto_class = []
 
 test_input = []
 test_output = []
 test_input_size = []
 test_output_size = []
+test_auto_class = []
+
+train_input = []
+train_output = []
+train_input_size = []
+train_output_size = []
+train_auto_class = []
 
 max_len = 30
 count = 0
 
-pattern = re.compile(r'\\.+\\')
+pattern = re.compile(r'\\.+')
 
-for concept in test_files:
+for concept in tqdm(train_files):
     files = glob(concept+'/*')
-    for file in files:
-        if 'json' not in file:
-            continue
-        with open(file, 'rb') as f:
-            data = json.load(f)
-        train_data = data['train']
-        valid_data = data['test']
+    with open(concept, 'rb') as f:
+        data = json.load(f)
+    train_data = data['train']
+    valid_data = data['test']
 
-        for i in range(len(train_data)):
-            train_data[i]['input'] = (np.array(train_data[i]['input'])).tolist()
-            train_input_y_len, train_input_x_len = np.array(train_data[i]['input']).shape
-            train_output_y_len, train_output_x_len = np.array(train_data[i]['output']).shape
-            input_size = (train_input_y_len, train_input_x_len)
-            output_size = (train_output_y_len, train_output_x_len)
+    for i in range(len(train_data)):
+        train_data[i]['input'] = (np.array(train_data[i]['input'])).tolist()
+        train_input_y_len, train_input_x_len = np.array(train_data[i]['input']).shape
+        train_output_y_len, train_output_x_len = np.array(train_data[i]['output']).shape
+        input_size = (train_input_y_len, train_input_x_len)
+        output_size = (train_output_y_len, train_output_x_len)
 
-            input_arr = [
-                [0 if m > train_input_x_len - 1 or n > train_input_y_len - 1 else train_data[i]['input'][n][m] + 1 for m
-                 in range(max_len)] for n in range(max_len)]
-            output_arr = [
-                [0 if m > train_output_x_len - 1 or n > train_output_y_len - 1 else train_data[i]['output'][n][m] + 1
-                 for m in range(max_len)] for n in range(max_len)]
+        input_arr = [
+            [0 if m > train_input_x_len - 1 or n > train_input_y_len - 1 else train_data[i]['input'][n][m] + 1 for m
+                in range(max_len)] for n in range(max_len)]
+        output_arr = [
+            [0 if m > train_output_x_len - 1 or n > train_output_y_len - 1 else train_data[i]['output'][n][m] + 1
+                for m in range(max_len)] for n in range(max_len)]
 
-            # train_input.append(input_arr)
-            test_auto_dataset.append(input_arr)
-            # train_output.append(output_arr)
-            test_auto_dataset.append(output_arr)
+        train_input.append(input_arr)
+        # test_auto_dataset.append(input_arr)
+        train_output.append(output_arr)
+        # test_auto_dataset.append(output_arr)
 
-            # train_input_size.append(input_size)
-            test_auto_size.append(input_size)
-            # train_output_size.append(output_size)
-            test_auto_size.append(output_size)
+        train_input_size.append(input_size)
+        # test_auto_size.append(input_size)
+        train_output_size.append(output_size)
+        # test_auto_size.append(output_size)
 
-            a = pattern.search(file)
-            data_class = file[a.regs[0][0]:a.regs[0][1]].strip('\\')
-            test_auto_class.append(data_class)
-            test_auto_class.append(data_class)
+        a = pattern.search(concept)
+        data_class = concept[a.regs[0][0]:a.regs[0][1]].strip('\\').split('.')[0][:-1] if '10' not in concept[a.regs[0][0]:a.regs[0][1]].strip('\\').split('.')[0] else concept[a.regs[0][0]:a.regs[0][1]].strip('\\').split('.')[0][:-2]
+        train_auto_class.append(data_class)
+        train_auto_class.append(data_class)
 
-        for i in range(len(valid_data)):
-            valid_data[i]['input'] = (np.array(valid_data[i]['input'])).tolist()
-            valid_input_y_len, valid_input_x_len = np.array(valid_data[i]['input']).shape
-            valid_output_y_len, valid_output_x_len = np.array(valid_data[i]['output']).shape
+    for i in range(len(valid_data)):
+        valid_data[i]['input'] = (np.array(valid_data[i]['input'])).tolist()
+        valid_input_y_len, valid_input_x_len = np.array(valid_data[i]['input']).shape
+        valid_output_y_len, valid_output_x_len = np.array(valid_data[i]['output']).shape
 
-            input_size = (valid_input_y_len, valid_input_x_len)
-            output_size = (valid_output_y_len, valid_output_x_len)
+        input_size = (valid_input_y_len, valid_input_x_len)
+        output_size = (valid_output_y_len, valid_output_x_len)
 
-            input_arr = [
-                [0 if m > valid_input_x_len - 1 or n > valid_input_y_len - 1 else valid_data[i]['input'][n][m] + 1 for m
-                 in range(max_len)] for n in range(max_len)]
-            output_arr = [
-                [0 if m > valid_output_x_len - 1 or n > valid_output_y_len - 1 else valid_data[i]['output'][n][m] + 1
-                 for m in range(max_len)] for n in range(max_len)]
+        input_arr = [
+            [0 if m > valid_input_x_len - 1 or n > valid_input_y_len - 1 else valid_data[i]['input'][n][m] + 1 for m
+                in range(max_len)] for n in range(max_len)]
+        output_arr = [
+            [0 if m > valid_output_x_len - 1 or n > valid_output_y_len - 1 else valid_data[i]['output'][n][m] + 1
+                for m in range(max_len)] for n in range(max_len)]
 
-            # valid_input.append(input_arr)
-            test_auto_dataset.append(input_arr)
-            # valid_output.append(output_arr)
-            test_auto_dataset.append(output_arr)
+        train_input.append(input_arr)
+        # test_auto_dataset.append(input_arr)
+        train_output.append(output_arr)
+        # test_auto_dataset.append(output_arr)
 
-            # valid_input_size.append(input_size)
-            test_auto_size.append(input_size)
-            # valid_output_size.append(output_size)
-            test_auto_size.append(output_size)
+        train_input_size.append(input_size)
+        # test_auto_size.append(input_size)
+        train_output_size.append(output_size)
+        # test_auto_size.append(output_size)
 
-            a = pattern.search(file)
-            data_class = file[a.regs[0][0]:a.regs[0][1]].strip('\\')
-            test_auto_class.append(data_class)
-            test_auto_class.append(data_class)
+        a = pattern.search(concept)
+        data_class = concept[a.regs[0][0]:a.regs[0][1]].strip('\\').split('.')[0][:-1] if '10' not in concept[a.regs[0][0]:a.regs[0][1]].strip('\\').split('.')[0] else concept[a.regs[0][0]:a.regs[0][1]].strip('\\').split('.')[0][:-2]
+        train_auto_class.append(data_class)
+        train_auto_class.append(data_class)
+
+for concept in tqdm(test_files):
+    with open(concept, 'rb') as f:
+        data = json.load(f)
+    train_data = data['train']
+    valid_data = data['test']
+
+    for i in range(len(train_data)):
+        train_data[i]['input'] = (np.array(train_data[i]['input'])).tolist()
+        train_input_y_len, train_input_x_len = np.array(train_data[i]['input']).shape
+        train_output_y_len, train_output_x_len = np.array(train_data[i]['output']).shape
+        input_size = (train_input_y_len, train_input_x_len)
+        output_size = (train_output_y_len, train_output_x_len)
+
+        input_arr = [
+            [0 if m > train_input_x_len - 1 or n > train_input_y_len - 1 else train_data[i]['input'][n][m] + 1 for m
+                in range(max_len)] for n in range(max_len)]
+        output_arr = [
+            [0 if m > train_output_x_len - 1 or n > train_output_y_len - 1 else train_data[i]['output'][n][m] + 1
+                for m in range(max_len)] for n in range(max_len)]
+
+        test_input.append(input_arr)
+        # test_auto_dataset.append(input_arr)
+        test_output.append(output_arr)
+        # test_auto_dataset.append(output_arr)
+
+        test_input_size.append(input_size)
+        # test_auto_size.append(input_size)
+        test_output_size.append(output_size)
+        # test_auto_size.append(output_size)
+
+        a = pattern.search(concept)
+        data_class = concept[a.regs[0][0]:a.regs[0][1]].strip('\\').split('.')[0][:-1] if '10' not in concept[a.regs[0][0]:a.regs[0][1]].strip('\\').split('.')[0] else concept[a.regs[0][0]:a.regs[0][1]].strip('\\').split('.')[0][:-2]
+        test_auto_class.append(data_class)
+        test_auto_class.append(data_class)
+
+    for i in range(len(valid_data)):
+        valid_data[i]['input'] = (np.array(valid_data[i]['input'])).tolist()
+        valid_input_y_len, valid_input_x_len = np.array(valid_data[i]['input']).shape
+        valid_output_y_len, valid_output_x_len = np.array(valid_data[i]['output']).shape
+
+        input_size = (valid_input_y_len, valid_input_x_len)
+        output_size = (valid_output_y_len, valid_output_x_len)
+
+        input_arr = [
+            [0 if m > valid_input_x_len - 1 or n > valid_input_y_len - 1 else valid_data[i]['input'][n][m] + 1 for m
+                in range(max_len)] for n in range(max_len)]
+        output_arr = [
+            [0 if m > valid_output_x_len - 1 or n > valid_output_y_len - 1 else valid_data[i]['output'][n][m] + 1
+                for m in range(max_len)] for n in range(max_len)]
+
+        test_input.append(input_arr)
+        # test_auto_dataset.append(input_arr)
+        test_output.append(output_arr)
+        # test_auto_dataset.append(output_arr)
+
+        test_input_size.append(input_size)
+        # test_auto_size.append(input_size)
+        test_output_size.append(output_size)
+        # test_auto_size.append(output_size)
+
+        a = pattern.search(concept)
+        data_class = concept[a.regs[0][0]:a.regs[0][1]].strip('\\').split('.')[0][:-1] if '10' not in concept[a.regs[0][0]:a.regs[0][1]].strip('\\').split('.')[0] else concept[a.regs[0][0]:a.regs[0][1]].strip('\\').split('.')[0][:-2]
+        test_auto_class.append(data_class)
+        test_auto_class.append(data_class)
+
+
+train_auto_json_data = {
+    'input': train_input,
+    'output': train_output,
+    'input_size': train_input_size,
+    'output_size': train_output_size,
+    'task': train_auto_class,
+}
 
 
 test_auto_json_data = {
-    'data': test_auto_dataset,
-    'size': test_auto_size,
-    'class': test_auto_class
+    'input': test_input,
+    'output': test_output,
+    'input_size': test_input_size,
+    'output_size': test_output_size,
+    'task': test_auto_class,
 }
 
-with open(f'{dataset_path}/test_concept_auto_data.json', 'w') as f:
+with open(f'{dataset_path}/train_concept.json', 'w') as f:
+    json.dump(train_auto_json_data, f)
+
+with open(f'{dataset_path}/test_concept.json', 'w') as f:
     json.dump(test_auto_json_data, f)
